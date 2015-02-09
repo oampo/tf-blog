5b0e3204a2e1e2af583fefd37f22c0747aaf703b
Joe Turner <joe@oampo.co.uk>
2015-02-09 10:12:08 +0000

    Switch to Postgres.

--- blog/config.py
+++ blog/config.py
@@ -1,12 +1,12 @@
 from secret import SECRET_KEY
 
 class DevelopmentConfig(object):
-    SQLALCHEMY_DATABASE_URI = "sqlite:///blog-development.db"
+    SQLALCHEMY_DATABASE_URI = "postgresql://action:action@localhost:5432/blogful"
     DEBUG = True
     SECRET_KEY = SECRET_KEY
 
 class TestingConfig(object):
-    SQLALCHEMY_DATABASE_URI = "sqlite:///blog-testing.db"
+    SQLALCHEMY_DATABASE_URI = "postgresql://localhost:5432/blogful"
     DEBUG = False
     SECRET_KEY = "Not secret"
 
